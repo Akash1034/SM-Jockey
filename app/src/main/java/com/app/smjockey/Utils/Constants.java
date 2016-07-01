5513dbcde0d832a2cda631286ebf794c004a4e2d
Alok Srivastava <akashsesampark@gmail.com>
2016-07-01 18:47:59 +0530

    Stream Bugs resolved

--- app/src/main/java/com/app/smjockey/Utils/Constants.java
+++ app/src/main/java/com/app/smjockey/Utils/Constants.java
@@ -8,7 +8,6 @@ public class Constants {
     public static int SPLASH_TIME_OUT = 3000;
     public static String auth_url="https://smjockey.co/api/v1/token";
     public static String AUTH_REQUEST_TAG="Authorization Request";
-    public static String user_details_url="https://smjockey.co/api/v1/me";
     public static String GET_REQUEST_TAG="User Details Request";
     public static String TOKEN_FILE="UserToken";
     public static String streams_url="https://smjockey.co/api/v1/streams/?page=";
