d0dcb2c5dc20dfd0a6bd3b9678bd230275336726
Alok Srivastava <akashsesampark@gmail.com>
2016-07-09 18:53:20 +0530

    Multi Select Remaining

--- app/src/main/java/com/app/smjockey/Activities/StreamActivity.java
+++ app/src/main/java/com/app/smjockey/Activities/StreamActivity.java
@@ -1,9 +1,9 @@
 package com.app.smjockey.Activities;
 
 import android.content.SharedPreferences;
+import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
@@ -19,7 +19,6 @@ import com.app.smjockey.R;
 import com.app.smjockey.Utils.Constants;
 import com.app.smjockey.Volley.NetworkCalls;
 import com.app.smjockey.Volley.Responses;
-import com.firebase.client.Firebase;
 
 import org.json.JSONArray;
 import org.json.JSONException;
